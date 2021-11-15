d028d3b85eacd5750c4df7382e8ad9d11ad3ff73
Jonas Holbech <jh@jonasholbech.dk>
2021-11-15 22:44:54 +0100

    initial auth

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import "./App.css";
-
+import { useEffect, useState } from "react";
+import netlifyIdentity from "netlify-identity-widget";
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [token, setToken] = useState("");
+  useEffect(() => {
+    async function setup() {
+      netlifyIdentity.init();
+      console.log("init");
+      netlifyIdentity.on("init", async (user) => {
+        console.log("init ran");
+        if (user) {
+          //await listTasks()//or similar
+          setToken(user.token.access_token);
+          setIsLoggedIn(true);
+        }
+      });
+      netlifyIdentity.on("login", async (user) => {
+        console.log("login ran");
+        //await listTasks()//or similar
+        setToken(user.token.access_token);
+        setIsLoggedIn(true);
+      });
+      netlifyIdentity.on("logout", () => {
+        console.log("logout ran");
+        setToken("");
+        setIsLoggedIn();
+      });
+    }
+    setup();
+
+    //created event, useEffect, onMount etc
+  }, []);
   return (
     <div className="App">
-      <div data-netlify-identity-menu></div>
+      <button onClick={() => netlifyIdentity.open()}>Login</button>
+      {token}
     </div>
   );
 }
